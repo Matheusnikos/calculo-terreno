8069351bc9271561281942fcff8e546cb20689e8
Matheus Nikos <matheusnikos.izaias@gmail.com>
2023-10-15 18:04:57 -0300

    inputs adicionados

--- 1-calcular-terreno.dart
+++ 1-calcular-terreno.dart
@@ -1,8 +1,36 @@
-main() {
-  num largura = 32;
-  num comprimento = 10;
+import 'dart:io';
 
-  num areaTerreno = largura * comprimento;
+void main() {
+  print("\x1B[2J\x1B[0;0H");
 
-  print('Área total do terreno = $areaTerreno' 'm');
+  num largura = pegarLargura();
+  print("\x1B[2J\x1B[0;0H");
+
+  num comprimento = pegarComprimento();
+  print("\x1B[2J\x1B[0;0H");
+
+  num resultado = largura * comprimento;
+
+  print('Área total do terreno = $resultado' 'm');
+  print('Fim do programa');
+}
+
+num pegarLargura() {
+  print('Digite a largura do terreno');
+  String? larguraString = stdin.readLineSync();
+  if (larguraString == null) {
+    return 0.0;
+  } else {
+    return num.parse(larguraString);
+  }
+}
+
+num pegarComprimento() {
+  print('Digite o comprimento do terreno');
+  String? comprimentoString = stdin.readLineSync();
+  if (comprimentoString == null) {
+    return 0.0;
+  } else {
+    return num.parse(comprimentoString);
+  }
 }
